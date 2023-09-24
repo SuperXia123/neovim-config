222b81425924d70ecd07c1ea445de75fded5ab12
yufeng.xia <xiayufenfeng@163.com>
2023-09-24 14:13:11 +0800

    chore(nvim): Add custom and plugin folder to git

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+-- In order to disable a default keymap, use
+M.disabled = {
+--   n = {
+--       ["<leader>h"] = "",
+--       ["<C-a>"] = ""
+--   }
+}
+
+-- Your custom mappings
+M.abc = {
+  n = {
+     -- ["<C-n>"] = {"<cmd> Telescope <CR>", "Telescope"},
+     -- ["<C-s>"] = {":Telescope Files <CR>", "Telescope Files"} 
+     ["j"] = { "jzz", "jump down and focus" , opts = { nowait = true }},
+     ["k"] = { "kzz", "jump up and focus" , opts = { nowait = true }},
+  },
+
+  i = {
+     ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+    -- ...
+  }
+}
+
+return M
