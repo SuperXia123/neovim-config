d5deb1f7f10f3edf549fd47fd2fad04cdc91182b
yufeng.xia <xiayufenfeng@163.com>
2023-10-04 02:15:08 +0800

    config spell-checking

---
+++ lua/custom/configs/spellchecker-config.lua
@@ -0,0 +1,34 @@
+-- 使能拼写检查
+vim.opt.spell = true
+-- 使能根据syntax的着色与高亮
+vim.cmd("syntax on")
+-- Chinese/Japanese/Korean拼写检查关闭
+vim.opt.spelllang = {'en_us', 'cjk'}
+-- 使能驼峰变量的spell-check
+vim.opt.spelloptions = 'camel'
+
+local autocmd = vim.api.nvim_create_autocmd
+local spell_augroup = vim.api.nvim_create_augroup("spell_augroup", { clear = true })
+-- 关闭对指定文件类型的spell-check
+autocmd("FileType", {
+  pattern = { "json", "lua" },
+  command = "setlocal nospell",
+  group = spell_augroup,
+})
+-- 关闭terminal的spell-check
+autocmd("TermOpen", {
+  pattern = "*",
+  command = "setlocal nospell",
+  group = spell_augroup,
+})
+-- 允许对语法单词做拼写检查
+-- autocmd设置在Syntax配置后执行此命令以覆盖
+-- see https://vi.stackexchange.com/questions/21492/enable-spelling-in-code-files-outside-of-comments
+autocmd({ "Syntax" }, {
+  pattern = "*",
+  callback = function()
+    vim.cmd "syntax spell toplevel"
+  end,
+  group = spell_augroup,
+})
+
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,6 @@
 -- 设置代码对齐线
 vim.opt.colorcolumn = "80"
 
+-- 加载拼写检查设置
+require("custom.configs.spellchecker-config")
+
