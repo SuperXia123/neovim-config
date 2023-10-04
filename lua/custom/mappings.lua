35e0a1ec20e5dff17e0a58e4dd053f95d6e136c4
yufeng.xia <xiayufenfeng@163.com>
2023-10-05 01:33:19 +0800

    use telescope symbols outline

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,6 +1,5 @@
-function SymbolsSearch()
-  vim.cmd 'AerialOpen'
-  vim.cmd 'Telescope current_buffer_fuzzy_find'
+function SearchSymbols()
+  vim.cmd "Telescope lsp_document_symbols symbol_width=60 show_line=false"
 end
 
 local M = {}
@@ -16,26 +15,16 @@ M.disabled = {
 -- Your custom mappings
 M.abc = {
   n = {
-     -- ["<C-n>"] = {"<cmd> Telescope <CR>", "Telescope"},
-     -- ["<C-s>"] = {":Telescope Files <CR>", "Telescope Files"} 
-     ["<F12>"] = {":lua SymbolsSearch() <CR>", "symbols outline"},
-     ["j"] = { "jzz", "jump down and focus" , opts = { nowait = true }},
-     ["k"] = { "kzz", "jump up and focus" , opts = { nowait = true }},
+    -- 使用telescope搜索文件大纲
+    -- about symbols_width and show_line see: 
+    -- https://www.reddit.com/r/neovim/comments/13f425s/help_needed_with_telescopebuiltinlsp_document/
+    ["<F12>"] = {":lua SearchSymbols() <CR>", "symbols outline"},
+    ["j"] = { "jzz", "jump down and focus" , opts = { nowait = true }},
+    ["k"] = { "kzz", "jump up and focus" , opts = { nowait = true }},
   },
 
   i = {
      ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
-    -- ...
-  }
-}
-
-M.symbols_outline = {
-  n = {
-
-  },
-
-  i = {
-
   }
 }
 
