4f8b57ecb98705147eb5b8944dcc175bdbcc1672
yufeng.xia <xiayufenfeng@163.com>
2023-10-05 17:04:49 +0800

    exrc in init.lua

---
+++ lua/custom/configs/dap/dap-config.lua
@@ -0,0 +1,13 @@
+local dap = require "dap"
+
+-- cpp的dap.adaptor配置
+local cpp_debugger_port = "13000"
+dap.adapters.codelldb = {
+  name = "codelldb server",
+  type = "server",
+  port = cpp_debugger_port,
+  executable = {
+    command = vim.fn.stdpath "data" .. "/mason/bin/codelldb",
+    args = { "--port", cpp_debugger_port },
+  },
+}
