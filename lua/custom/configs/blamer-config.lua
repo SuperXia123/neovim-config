fc03b90ef3c6473a92d8b6c4f7070348b7f436e6
yufeng.xia <xiayufenfeng@163.com>
2023-10-06 18:04:08 +0800

    proj-specific configs

--- lua/custom/configs/blamer-config.lua
+++ lua/custom/configs/blamer-config.lua
@@ -6,6 +6,6 @@ vim.g.blamer_date_format = '%y/%m/%d'
 vim.g.blamer_relative_time = 1
 vim.cmd "highlight Blamer guifg=lightgrey"
 vim.g.blamer_delay = 0
-vim.g.blamer_show_in_visual_modes = 1
+vim.g.blamer_show_in_visual_modes = 0
 vim.g.blamer_show_in_insert_modes = 1
 
--- lua/custom/configs/spellchecker-config.lua
+++ lua/custom/configs/spellchecker-config.lua
@@ -1,13 +1,13 @@
 -- 使能拼写检查
 vim.opt.spell = true
 -- 使能根据syntax的着色与高亮
-vim.cmd("syntax on")
+vim.cmd "syntax on"
 -- Chinese/Japanese/Korean拼写检查关闭
-vim.opt.spelllang = {'en_us', 'cjk'}
+vim.opt.spelllang = { "en_us", "cjk" }
 -- 使能驼峰变量的spell-check
-vim.opt.spelloptions = 'camel'
+vim.opt.spelloptions = "camel"
 -- 关闭首字母大写检查, see https://neovim.io/doc/user/options.html#'spellcapcheck'
-vim.opt.spellcapcheck = ''
+vim.opt.spellcapcheck = ""
 
 local autocmd = vim.api.nvim_create_autocmd
 local spell_augroup = vim.api.nvim_create_augroup("spell_augroup", { clear = true })
@@ -33,4 +33,3 @@ autocmd({ "Syntax" }, {
   end,
   group = spell_augroup,
 })
-
