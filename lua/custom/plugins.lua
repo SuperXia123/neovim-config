ce0244536efcb6822fe604c2ad134f7a844ff31d
yufeng.xia <xiayufenfeng@163.com>
2023-10-11 10:47:18 +0800

    config lsp using mason-lspconfig

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,8 +2,32 @@ local plugins = {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      -- 加载lsp的key-mapping
+      require("core.utils").load_mappings "lspconfig"
+
+      require("mason-lspconfig").setup {
+        ensure_installed = { "clangd" },
+      }
+
+      require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+        -- ["clangd"] = function()
+        --   require("lspconfig").clangd.setup {}
+        -- end,
+      }
     end,
   },
   {
