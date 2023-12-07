40e708cb1dc63f13c04fdceaea40b3eb877575b6
yufeng.xia <xiayufenfeng@163.com>
2023-12-07 14:47:57 +0800

    config proto-lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,16 +1,21 @@
+-- see supported lsps here:
+-- ~/.local/share/nvim/lazy/nvim-lspconfig/lua/lspconfig/server_configurations
 local base = require("plugins.configs.lspconfig")
 local on_attach = base.on_attach
 local capacities = base.capabilities
 
 local lspconfig = require("lspconfig")
+
 lspconfig.clangd.setup {
   on_attach = function (client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
   end,
   capacities = capacities,
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
 }
 
-lspconfig.pyright.setup {
-  on_attach
-}
+lspconfig.pyright.setup {}
+
+lspconfig.bufls.setup {}
+
