bb18e636ccd67369f0cc7c3a53c992b7b78dd4b7
yufeng.xia <xiayufenfeng@163.com>
2023-12-24 01:11:12 +0800

    dap config adjustment

--- lua/custom/configs/dap/dap-symbols.lua
+++ lua/custom/configs/dap/dap-symbols.lua
@@ -2,7 +2,7 @@ vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#db5c5c", bg = "" }
 vim.api.nvim_set_hl(0, "DapBreakLine", { ctermbg = 0, fg = "", bg = "#4b1515"})
 vim.api.nvim_set_hl(0, "DapStoppedLine", { ctermbg = 0, fg = "", bg = "#264b33" })
 
-local brealpoint_symbol = ""
+local brealpoint_symbol = ""
 
 vim.fn.sign_define(
   "DapBreakpoint",
