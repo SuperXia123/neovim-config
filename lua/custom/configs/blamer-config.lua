e55ba385afa3b9772f1addb245e86232b56b9927
yufeng.xia <xiayufenfeng@163.com>
2024-01-04 19:31:49 +0800

    adjust blamer config

--- lua/custom/configs/blamer-config.lua
+++ lua/custom/configs/blamer-config.lua
@@ -1,11 +1,11 @@
 -- blamer.git设置
 vim.g.blamer_enabled = ENABLE_BLAMER
-vim.g.blamer_prefix = '    '
+vim.g.blamer_prefix = '        '
 vim.g.blamer_template = "<committer> (<commit-short>), <committer-time> • <summary>"
 vim.g.blamer_date_format = '%y/%m/%d'
 vim.g.blamer_relative_time = 1
 vim.cmd "highlight Blamer guifg=lightgrey"
 vim.g.blamer_delay = 0
 vim.g.blamer_show_in_visual_modes = 0
-vim.g.blamer_show_in_insert_modes = 1
+vim.g.blamer_show_in_insert_modes = 0
 
